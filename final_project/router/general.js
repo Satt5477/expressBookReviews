32e53ac248f6623e9353340477f2851264c9346e
satriadi.purnama@gmail.com <satriadi.purnama@pentacom-asia.com>
2025-03-19 18:52:05 +0700

    update with course final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,37 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here    
+  const authorparam = req.params.author;
+  const filteredBooks = Object.values(books).filter(book => book.author === authorparam);
+    res.send(filteredBooks);  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const titleparam = req.params.title;
+  const filteredBooks = Object.values(books).filter(book => book.title === titleparam);
+    res.send(filteredBooks);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn]);
 });
 
 module.exports.general = public_users;
